3b989ba32cea0c0225271075810311e71022d7a9
Emeka1993 <brolin933@gmail.com>
2022-10-11 16:37:20 +0100

    Mandatory

--- mandatory/debugging-practice/script.js
+++ mandatory/debugging-practice/script.js
@@ -1,19 +1,22 @@
 let myLibrary = [];
 
-window.addEventListener("load", function (e) {
+window.addEventListener("load", function (e)
+{
   populateStorage();
   render();
 });
 
-function populateStorage() {
-  if (myLibrary.length == 0) {
+function populateStorage()
+{
+  if (myLibrary.length == 0)
+  {
     let book1 = new Book("Robison Crusoe", "Daniel Defoe", "252", true);
-    let book2 = new Book(
-      "The Old Man and the Sea",
+
       "Ernest Hemingway",
       "127",
       true
     );
+    let book2 = new Book("The Old Man and the Sea", "Ernest Hemingway", "127", true);
     myLibrary.push(book1);
     myLibrary.push(book2);
     render();
@@ -29,6 +32,7 @@ const check = document.getElementById("check");
 //via Book function and start render function
 function submit() {
   if (
+    
     title.value == null ||
     title.value == "" ||
     pages.value == null ||
@@ -54,12 +58,12 @@ function render() {
   let table = document.getElementById("display");
   let rowsNumber = table.rows.length;
   //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- {
+  for (let n = rowsNumber - 1; n >= 0; n--) {
     table.deleteRow(n);
   }
   //insert updated row and cells
-  let length = myLibrary.length;
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let row = table.insertRow(i);
     let row = table.insertRow(1);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
@@ -76,7 +80,7 @@ function render() {
     changeBut.className = "btn btn-success";
     cell4.appendChild(changeBut);
     let readStatus = "";
-    if (myLibrary[i].check == false) {
+    if (myLibrary[i].check == true) {
       readStatus = "Yes";
     } else {
       readStatus = "No";
@@ -90,7 +94,11 @@ function render() {
 
     //add delete button to every row and render again
     let delButton = document.createElement("button");
-    delBut.id = i + 5;
+    delButton.id = i + 5;
+    cell5.appendChild(delButton);
+    delButton.className = "btn btn-warning";
+    delButton.innerHTML = "Delete";
+    delButton.addEventListener("click", function () {
     cell5.appendChild(delBut);
     delBut.className = "btn btn-warning";
     delBut.innerHTML = "Delete";
